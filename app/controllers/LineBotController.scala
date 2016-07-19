2632c4a447d31d97fdf9d3f2e86457c877404816
abcdefuji <kengo_fujishiro@dwango.co.jp>
2016-07-19 17:21:45 +0900

    echo

--- app/controllers/LineBotController.scala
+++ app/controllers/LineBotController.scala
@@ -1,17 +1,66 @@
 package controllers
 
+import config.LineBotConfig
+import java.net.URL
+import javax.inject._
+import play.api.Logger
+import play.api.libs.json.JsValue
 import play.api.libs.json.Json
+import play.api.libs.ws.DefaultWSProxyServer
+import play.api.libs.ws.WSClient
 import play.api.mvc.Action
-import play.api.mvc.AnyContent
-import play.api.mvc.Results
+import play.api.mvc._
+import scala.concurrent.ExecutionContext.Implicits.global
+import sun.misc.BASE64Encoder
 
-class LineBotController {
+@Singleton
+class LineBotController @Inject()(ws: WSClient) {
 
-  def echo: Action[AnyContent] = Action { request =>
-    println(Json.prettyPrint(request.body.asJson.get))
-    Results.Ok(views.html.index())
+  def echo: Action[JsValue] = Action.async(BodyParsers.parse.json) { data =>
+
+    val content = (data.body \\ "content").head
+    val exFrom = (content \ "from").get.as[String]
+    val exText = (content \ "text").get.as[String]
+
+    val body = Json.parse(
+      s"""{
+         |  "to":["$exFrom"],
+         |  "toChannel":1383378250,
+         |  "eventType":"138311608800106203",
+         |  "content":{
+         |    "contentType":1,
+         |    "toType":1,
+         |    "text":"$exText"
+         |  }
+         |}
+      """.stripMargin)
+
+    val encodedAuth = new BASE64Encoder().encode(
+      new URL(LineBotConfig.ProxyURL).getUserInfo.getBytes()
+    )
+
+    ws.url("https://trialbot-api.line.me/v1/events")
+      .withHeaders(
+        "Proxy-Authorization" -> s"Basic $encodedAuth",
+        "Content-Type" -> "application/json; charset=UTF-8",
+        "X-Line-ChannelID" -> LineBotConfig.ChannelID,
+        "X-Line-ChannelSecret" -> LineBotConfig.ChannelSecret,
+        "X-Line-Trusted-User-With-ACL" -> LineBotConfig.MID
+      )
+      .withProxyServer(
+        DefaultWSProxyServer(
+          host = "velodrome.usefixie.com",
+          port = 80,
+          principal = Some("fixie"),
+          password = Some("gbq3H6Vuqj71gXS")
+        )
+      ).post(body).map { res =>
+      Logger.info(res.body.toString())
+      Results.Ok
+    }
   }
 }
+
 case class LineMessage(
   from: String,
   fromChannel: String,
@@ -22,7 +71,8 @@ case class LineMessage(
   content: String
 )
 
-case class EventType private (value: String)
+case class EventType private(value: String)
+
 object EventType {
   val ReceiveMessage = EventType("138311609000106303")
   val UserAction = EventType("138311609100106403")
